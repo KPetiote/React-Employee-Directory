135c59f7bdedd6b794d98397f16557afc3708104
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-07-14 18:44:04 -0400

    Changed Header description

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <div className="text-center bg-success p-4">
             <h1 style={{ fontSize: 55, color: "white" }}>Employee Directory</h1>
-            <p style={{ color: "white" }}>Employee Details</p>
+            <p style={{ color: "white" }}>Please select the "A-Z" icon to sort names alphabetically</p>
         </div>
     )
 }
