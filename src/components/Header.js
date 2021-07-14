7f9f150d3440b39cc2679b94f057ef242dc833e2
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-14 08:27:25 -0400

    Added src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="text-center bg-info p-5">
+            <h1 style={{ fontSize: 60, color: "white" }}>Employee Directory</h1>
+            <p style={{ color: "white" }}>Employee Details</p>
+        </div>
+    )
+}
+
+export default Header;
