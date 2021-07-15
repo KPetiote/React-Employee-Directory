d58089d9cf4a8dfc3408fcbc2ff62c6bbef75dd4
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-14 20:16:00 -0400

    Modified scr/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ function Header() {
     return (
         <div className="text-center bg-success p-4">
             <h1 style={{ fontSize: 55, color: "white" }}>Employee Directory</h1>
-            <p style={{ color: "white" }}>Please select the "A-Z" icon to sort names alphabetically</p>
+            <p style={{ color: "white" }}>Please select the "A-Z" icon to sort by first name or "Z-A" to sort by last name</p>
+            
         </div>
     )
 }
